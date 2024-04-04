55b1a02461b78cad7cee666962706cdab979edea
mazamimspeed <105179045+mazamimspeed@users.noreply.github.com>
2024-04-04 13:50:53 +1100

    redirect

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+app.get("/details", (req, res) => {
+  res.redirect("https://www.adaptalift.com.au/");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
