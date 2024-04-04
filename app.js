b0dfa7ef10e0dc686646de22f9bd41b6bb2aa07c
mazamimspeed <105179045+mazamimspeed@users.noreply.github.com>
2024-04-04 14:33:51 +1100

    contents

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// // Serve static files from the 'public' directory
+// app.use(express.static("public"));
+
+// // Serve 'basicindex.html' when the root URL is accessed
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+
+// app.get("/details", (req, res) => {
+//   res.redirect(
+//     "https://www.adaptalift.com.au/new-equipment/h1-50-3-5xt-series-standard-forklifts"
+//   );
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -5,15 +27,33 @@ const port = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
-// Serve 'basicindex.html' when the root URL is accessed
+// Serve 'index.html' when the root URL is accessed
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+// Serve a special page that attempts to open the deep link
 app.get("/details", (req, res) => {
-  res.redirect(
-    "https://www.adaptalift.com.au/new-equipment/h1-50-3-5xt-series-standard-forklifts"
-  );
+  const deepLink = `com.example.deeplink_cookbook://details?id=${req.query.id}`;
+  const fallbackUrl =
+    "https://www.adaptalift.com.au/new-equipment/h1-50-3-5xt-series-standard-forklifts";
+  const htmlContent = `
+    <html>
+      <head>
+        <meta http-equiv="refresh" content="3;url=${fallbackUrl}">
+      </head>
+      <body>
+        <script type="text/javascript">
+          window.onload = function() {
+            // Try to open the deep link
+            window.location = '${deepLink}';
+          }
+        </script>
+        If you are not redirected automatically, follow the <a href="${fallbackUrl}">link</a>.
+      </body>
+    </html>
+  `;
+  res.send(htmlContent);
 });
 
 app.listen(port, () => {
