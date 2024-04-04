36f93c7c0a3a833bd33adace8fdda1a67d98d8c9
mazamimspeed <105179045+mazamimspeed@users.noreply.github.com>
2024-04-04 15:10:53 +1100

    deep

--- app.js
+++ app.js
@@ -24,40 +24,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static("public"));
 
-// Serve 'index.html' when the root URL is accessed
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-// Intermediate endpoint to handle deep linking
 app.get("/details", (req, res) => {
-  const deepLink = `com.example.deeplink_cookbook://details?id=${req.query.id}`;
-  const fallbackUrl =
-    "https://www.adaptalift.com.au/new-equipment/h1-50-3-5xt-series-standard-forklifts";
-  const htmlContent = `
-    <html>
-      <head>
-        <script type="text/javascript">
-          function handleDeepLink() {
-            // Attempt to open the deep link
-            window.location.href = '${deepLink}';
-            // After a short delay, check if the user is still on the page
-            setTimeout(function() {
-              // If the user is still here, redirect to the fallback URL
-              window.location.href = '${fallbackUrl}';
-            }, 3000); // Delay in milliseconds
-          }
-        </script>
-      </head>
-      <body onload="handleDeepLink()">
-        <p>If you are not redirected, <a href="${fallbackUrl}">click here</a>.</p>
-      </body>
-    </html>
-  `;
-  res.send(htmlContent);
+  res.sendFile(__dirname + "/public/deeplink.html");
 });
 
 app.listen(port, () => {
